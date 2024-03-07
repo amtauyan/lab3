9edb17ac56c18798fd72d09bf836d68649bc637c
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-03-07 14:16:35 +0800

    Updated guests

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -7,19 +7,19 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-use App\Controllers\Guests;
+use App\Controllers\Guests; 
 use App\Controllers\News; 
 use App\Controllers\Pages;
 
-$routes->get('guests', [Guests::class, 'index']);           // Add this line
+$routes->get('guests', [Guests::class, 'index']);
 $routes->get('guests/new', [Guests::class, 'new']);
-$routes->post('guests', [Guests::class, 'create']); 
+$routes->post('guests', [Guests::class, 'create']);
 $routes->get('guests/(:segment)', [Guests::class, 'show']);
 
 $routes->get('news', [News::class, 'index']);
 $routes->get('news/new', [News::class, 'new']);
-$routes->post('news', [News::class, 'create']);         
-$routes->get('news/(:segment)', [News::class, 'show']); 
+$routes->post('news', [News::class, 'create']);
+$routes->get('news/(:segment)', [News::class, 'show']);
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
