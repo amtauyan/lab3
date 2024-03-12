ec4241d34d73e1737a5d4c8b11feecc9d6ce5019
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-03-12 14:18:12 +0800

    try changes for guests to work

--- ci4/app/Views/pages/resources.php
+++ ci4/app/Views/pages/resources.php
@@ -6,7 +6,7 @@
             <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/home" class="font-effect-neon">Home</a>
             <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/about" class="font-effect-neon">About</a>
             <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
-            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
+            <a href="guests" class="font-effect-neon">Guests</a>
             <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/resources" class="font-effect-neon">References</a>
         </nav>
     </header>
