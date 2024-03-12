37954ca890e6e3f85c1fce1d4c14faaf4d27dff1
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-03-12 14:14:51 +0800

    changes in guests

--- ci4/app/Views/guests/index.php
+++ ci4/app/Views/guests/index.php
@@ -1,13 +1,13 @@
-<link rel="stylesheet" type="text/css" href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/css/style.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <body>
   <header class="header">
     <a href="#" class="logo">SaiDa</a>
     <nav class="navbar">
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/home" class="font-effect-neon">Home</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/about" class="font-effect-neon">About</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/resources" class="font-effect-neon">References</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/home" class="font-effect-neon">Home</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/about" class="font-effect-neon">About</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/resources" class="font-effect-neon">References</a>
     </nav>
   </header>
   <h2 class="Subtitle">Guest Lists</h2>
@@ -28,6 +28,6 @@
     <p>Unable to find any guest for you.</p>
     <?php endif ?>
     <br>
-    <button class="smaller white-button" onclick="window.location='https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/guests/new';">Be a new guest</button>
+    <button class="smaller white-button" onclick="window.location='https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/guests/new';">Be a new guest</button>
   </div>
-  <script src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/javascript/about.js"></script>
+  <script src="javascript/about.js"></script>
