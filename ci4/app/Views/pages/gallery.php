08b5ff02e7f282b6786a4ef7ed1cd918980ca46a
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-03-12 13:56:54 +0800

    Changes in gallery and home for cloud

--- ci4/app/Views/pages/gallery.php
+++ ci4/app/Views/pages/gallery.php
@@ -1,38 +1,38 @@
-<link rel="stylesheet" type="text/css" href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/css/gallery.css">
+<link rel="stylesheet" type="text/css" href="css/gallery.css">
 <body>
     <header class="header">
         <a href="#" class="logo">SaiDa</a>
         <nav class="navbar">
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/home" class="font-effect-neon">Home</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/about" class="font-effect-neon">About</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/resources" class="font-effect-neon">References</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/home" class="font-effect-neon">Home</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/about" class="font-effect-neon">About</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/resources" class="font-effect-neon">References</a>
         </nav>
     </header>
 
   <div class="polaroid-box">
     <h3 id="More">Angela's Dogs</h3>
     <div class="dogs">
-      <img src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/images/Toffee.jpg" alt="Brown Dachshund Dog Named Toffee" style="width:200px; height:300px;">
+      <img src="images/Toffee.jpg" alt="Brown Dachshund Dog Named Toffee" style="width:200px; height:300px;">
       <div class="container">
         <p class="polaroid">Toffee "Chubibo"</p>
       </div>
     </div>
     <div class="dogs">
-      <img src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/images/Rockee.jpg" alt="Brown and White Jack Russel Dog Named Rockee" style="width:200px; height:300px;">
+      <img src="images/Rockee.jpg" alt="Brown and White Jack Russel Dog Named Rockee" style="width:200px; height:300px;">
       <div class="container">
         <p class="polaroid">Rockee "Rockipo"</p>
       </div>
     </div>
     <div class="dogs">
-      <img src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/images/Peewee.jpg" alt="Black Dachshund Dog Named Peewee" style="width:200px; height:300px;">
+      <img src="images/Peewee.jpg" alt="Black Dachshund Dog Named Peewee" style="width:200px; height:300px;">
       <div class="container">
         <p class="polaroid">Peewee "Piwit"</p>
       </div>
     </div>
     <div class="dogs">
-      <img src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/images/Shortcake.jpg" alt="Brown and Black Dachshund Dog Named Shortcake" style="width:200px; height:300px;">
+      <img src="images/Shortcake.jpg" alt="Brown and Black Dachshund Dog Named Shortcake" style="width:200px; height:300px;">
       <div class="container">
         <p class="polaroid">Shortcake "Shorty"</p>
       </div>
@@ -42,17 +42,17 @@
   <div class="polaroid-box">
     <h3 id="More"> Angela's Nature Photographs</h3>
     <div class="nature">
-      <img src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/images/Talon.jpg" alt="Road with Trees on the Side" style="width:200px; height:300px;">
+      <img src="images/Talon.jpg" alt="Road with Trees on the Side" style="width:200px; height:300px;">
       <div class="container">
         <p class="polaroid">Crispina, Ilocos Norte</p>
       </div>
     </div>
     <div class="nature">
-      <img src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/images/Hugging Trees.jpg" alt="Tall Trees Bottom View" style="width:200px; height:300px;">
+      <img src="images/Hugging Trees.jpg" alt="Tall Trees Bottom View" style="width:200px; height:300px;">
       <div class="container">
         <p class="polaroid">Tall Trees</p>
       </div>
     </div>
   </div>
 
-  <script src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/javascript/gallery.js"></script>
+  <script src="javascript/gallery.js"></script>
--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -1,20 +1,20 @@
-<link rel="stylesheet" type="text/css" href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/css/home.css">
+<link rel="stylesheet" type="text/css" href="css/home.css">
 <body>
     <header class="header">
         <a href="#" class="logo">SaiDa</a>
         <nav class="navbar">
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/home" class="font-effect-neon">Home</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/about" class="font-effect-neon">About</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
-            <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/resources" class="font-effect-neon">References</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/home" class="font-effect-neon">Home</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/about" class="font-effect-neon">About</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
+            <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/resources" class="font-effect-neon">References</a>
         </nav>
     </header>
     <div class="text-box">
         <h1> SaiDa's <span class="auto-input"></h1>
         <p> IT Student · Sporty · Once · Blink · MY </p>
     </div>
-    <img src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/images/Profile.jpg" class="profile" alt="Portrait of Angela Tauyan">
+    <img src="images/Profile.jpg" class="profile" alt="Portrait of Angela Tauyan">
     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
     <script>
         var typed = new Typed(".auto-input", {
@@ -24,4 +24,4 @@
             loop: true
         })
     </script>
-    <script src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/javascript/home.js"></script>
+    <script src="javascript/home.js"></script>
