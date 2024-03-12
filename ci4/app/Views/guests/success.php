afb3395e11442c00b39a6f7a86513b0bf8fc35e6
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-03-12 14:37:46 +0800

    final rework for guests

--- ci4/app/Views/guests/success.php
+++ ci4/app/Views/guests/success.php
@@ -1,18 +1,18 @@
-<link rel="stylesheet" type="text/css" href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/css/style.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <body>
   <header class="header">
     <a href="#" class="logo">SaiDa</a>
     <nav class="navbar">
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/home" class="font-effect-neon">Home</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/about" class="font-effect-neon">About</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
-      <a href="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/resources" class="font-effect-neon">References</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/home" class="font-effect-neon">Home</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/about" class="font-effect-neon">About</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/gallery" class="font-effect-neon">Gallery</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/guests" class="font-effect-neon">Guests</a>
+      <a href="https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/resources" class="font-effect-neon">References</a>
     </nav>
   </header>
   <h2 class="Subtitle">Successful Create</h2>
   <div class="section-box">
     <p style="color:white;">Guests created successfully</p>
-    <button class="smaller white-button" onclick="window.location='https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/guests';">Go Back</button>
+    <button class="smaller white-button" onclick="window.location='https://apcwebprog.csf.ph/~amtauyan/lab3/ci4/public/guests';">Go Back</button>
   </div>
-  <script src="https://verbose-robot-6995g7vp6qppc49gp-8080.app.github.dev/lab3/ci4/public/javascript/about.js"></script>
+  <script src="javascript/about.js"></script>
